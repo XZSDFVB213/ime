6a9edace352d92633f8349b4f9e1ed106109baf6
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-09-15 00:28:30 +0300

    portfolio

--- ime-server/src/portfolio/dto/update-portfolio.dto.ts
+++ ime-server/src/portfolio/dto/update-portfolio.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreatePortfolioDto } from './create-portfolio.dto';
+import { CreatePortfolioItemDto } from './create-portfolio.dto';
 
-export class UpdatePortfolioDto extends PartialType(CreatePortfolioDto) {}
+export class UpdatePortfolioDto extends PartialType(CreatePortfolioItemDto) {}
